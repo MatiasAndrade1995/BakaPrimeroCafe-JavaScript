d28689a81f278524fbcd34edc64ca49775dcacd6
Matias Andrade <matiasandrade.unla@gmail.com>
2023-01-18 13:43:54 -0300

    Entrega Final, primeras correciones

--- js/main.js
+++ js/main.js
@@ -1,8 +1,34 @@
+//VARIABLES
+const $sectionIngresado = document.getElementById("sectionIngresado");
+const $sectionIngreso = document.getElementById("sectionIngreso");
+const $carrito = document.getElementById("carrito");
+const $contadorCarrito = document.getElementById("contadorCarrito");
+const $total = document.querySelector(".total");
+const $spinnerCarga = document.querySelector(".text-center");
+let productos;
+//FUNCIONES
+const getAllProductos = async () => {
+    try {
+        let res = await fetch("js/productos.json");
+        let json = await res.json();
+        if (res.ok == false) throw res;
+        productos = json.productos
+    } catch (error) {
+        console.log(error.statu)
+    }
+}
+const guardarProductosLS = (productos) => {
+    localStorage.setItem("productos", JSON.stringify(productos));
+}
+const cargarProductosLS = () => {
+    return JSON.parse(localStorage.getItem("productos")) || [];
+}
 const renderProductos = () => {
-    let productosHTML = "";
-    productos.forEach(element => {
-        productosHTML +=
-            `<div class="col-md-4 mb-4 d-flex justify-content-center">
+    setTimeout(() => {
+        let productosHTML = "";
+        productos.forEach(element => {
+            productosHTML +=
+                `<div class="col-md-4 mb-4 d-flex justify-content-center">
                 <div class="card text-center align-items-center border border-secondary" style="width: 18rem;">
                 <div class="contenedorImagen">
                     <img src="images/${element.imagen}" class="card-img-top" alt="${element.nombre}">
@@ -13,57 +39,45 @@ const renderProductos = () => {
                         <button class="btn btn-secondary" onClick="agregarAlCarrito(${element.id});">Comprar</button>
                     </div>
                 </div>
-            </div>`     
-    })
-    document.getElementById("productos").innerHTML = productosHTML
+            </div>`
+        })
+        document.getElementById("productos").innerHTML = productosHTML
+    }, 3000)
 }
-
-renderProductos(); //Muestra productos en HTML
-guardarProductosLS(productos); //Guarda productos en formato String de la Array "Productos"
-
-
 const guardarProductosCarrito = (productos) => {
     localStorage.setItem("carrito", JSON.stringify(productos));
 }
 
-const cargarProductosCarrito = () => {
-    return JSON.parse(localStorage.getItem("carrito")) || [];
-}
+const cargarProductosCarrito = () => JSON.parse(localStorage.getItem("carrito")) || []; //const "nombre de la función" = () => valor a retonar; Return implícito sin utilizar llaves ({}) en una sola linea de código.
 
 const agregarAlCarrito = (id) => {
     const productos = cargarProductosLS();
     const productos_carrito = cargarProductosCarrito();
     const producto = productos.find(element => element.id == id);
+    let elseExiste = () => {
+        productos_carrito[existe].cantidad += 1
+        productos_carrito[existe].precio = producto.precio * productos_carrito[existe].cantidad
+    }
     let existe = -1;
     productos_carrito.forEach((el, index) => {
         if (el.id == id) {
             existe = index;
         }
     })
-    if (existe == -1) {
-        productos_carrito.push(producto);
-    } else {
-        productos_carrito[existe].cantidad += 1
-        productos_carrito[existe].precio = producto.precio * productos_carrito[existe].cantidad
-    }
+    existe == -1 ? productos_carrito.push(producto) : elseExiste();
     guardarProductosCarrito(productos_carrito);
     renderProductos_carrito();
-}
-
-const quitarDelCarrito = (id) => {
-    let productos_carrito = cargarProductosCarrito();
-    productos_carrito = productos_carrito.filter((el) => {
-        return el.id != id;
-    })
-    guardarProductosCarrito(productos_carrito)
-    renderProductos_carrito()
 }
 
 const renderProductos_carrito = () => {
     let totalprecio = 0;
     let cantidadTotal = 0;
     let productosCarritoHTML = "";
-    let productos_carrito = cargarProductosCarrito()
+    let descuentoProductos = () => {
+        totalprecio -= ((totalprecio * 20) / 100)
+        $total.innerHTML = `<span class="enunciadoDescuento">¡Por comprar 5 productos o mas !</span><br>¡Total con 20% descuento: $${totalprecio}!`
+    }
+    const productos_carrito = cargarProductosCarrito()
     if (productos_carrito.length != 0) {
         productos_carrito.forEach(element => {
             totalprecio += element.precio
@@ -83,73 +97,161 @@ const renderProductos_carrito = () => {
                     </div>
             </div >`
         })
-        document.getElementById("carrito").innerHTML = productosCarritoHTML
+        $carrito.innerHTML = productosCarritoHTML;
     } else {
-        document.getElementById("carrito").innerHTML = `<p class="carritoVacio">¡Tu carrito está vacio!</p>`
+        $carrito.innerHTML = `<p class="carritoVacio">¡Tu carrito está vacio!</p>`
     }
-
-    if (cantidadTotal >= 5) {
-        totalprecio = totalprecio - ((totalprecio * 20) / 100)
-        document.querySelector(".total").innerHTML = `<span class="enunciadoDescuento">¡Por comprar 5 productos o mas !</span><br>¡Total con 20% descuento: $${totalprecio}!`
-    } else {
-        document.querySelector(".total").textContent = `Total: $${totalprecio}`
-    }
-    document.getElementById("contadorCarrito").textContent = cantidadTotal
+    cantidadTotal >= 5 ? descuentoProductos() : $total.textContent = `Total: $${totalprecio}`;
+    $contadorCarrito.textContent = cantidadTotal;
 }
 
-document.addEventListener("DOMContentLoaded", e => (renderProductos_carrito()))
+const quitarDelCarrito = (id) => {
+    let productos_carrito = cargarProductosCarrito();
+    productos_carrito = productos_carrito.filter((el) => el.id != id);
+    guardarProductosCarrito(productos_carrito);
+    renderProductos_carrito();
+}
 
 const vaciarCarrito = () => {
-    localStorage.removeItem("carrito")
-    document.getElementById("carrito").innerHTML = `<p class="carritoVacio">¡Tu carrito está vacio!</p>`
-    document.querySelector(".total").textContent = `Total: $${0}`
-    document.getElementById("contadorCarrito").textContent = 0
+    localStorage.removeItem("carrito");
+    $carrito.innerHTML = `<p class="carritoVacio">¡Tu carrito está vacio!</p>`;
+    $total.textContent = `Total: $${0}`;
+    $contadorCarrito.textContent = 0;
 }
-let botonVaciarCarrito = document.getElementById("vaciarCarrito")
 
-botonVaciarCarrito.addEventListener("click", e => {
-    if (e.target.matches("#vaciarCarrito")) {
-        vaciarCarrito()
-    }
-})
-
-
-// INGRESAR SESION
-let botonIngresar = document.getElementById("ingresar");
-botonIngresar.addEventListener("click", validarSesion);
-
+// INGRESAR SESIONCARRIT
 function validarSesion(e) {
     e.preventDefault();
-    let nombre = document.getElementById("nombre").value
-    let numeroCelular = document.getElementById("numeroCelular").value
-    let avisoCampoIncompletoNombre = document.getElementById("campoIncompletoNombre");
-    let avisoCampoIncompletoNumero = document.getElementById("campoIncompletoNumber");
+    const nombre = document.getElementById("nombre").value;
+    const numeroCelular = document.getElementById("numeroCelular").value;
+    const $avisoCampoIncompletoNombre = document.getElementById("campoIncompletoNombre");
+    const $avisoCampoIncompletoNumero = document.getElementById("campoIncompletoNumber");
     if (nombre == "") {
-        avisoCampoIncompletoNombre.innerHTML = "¡Ingrese un nombre en el campo!"
+        $avisoCampoIncompletoNombre.innerHTML = "¡Ingrese un nombre en el campo!"
         return false;
     } else {
-        avisoCampoIncompletoNombre.innerHTML = ""
+        $avisoCampoIncompletoNombre.innerHTML = ""
     }
     if (numeroCelular == "") {
-        avisoCampoIncompletoNumero.innerHTML = "¡Ingrese un número en el campo!"
+        $avisoCampoIncompletoNumero.innerHTML = "¡Ingrese un número en el campo!"
         return false;
     } else {
-        avisoCampoIncompletoNumero.innerHTML = ""
+        $avisoCampoIncompletoNumero.innerHTML = ""
     }
     if (nombre != "" && numeroCelular != "") {
-        let sectionIngresado = document.getElementById("sectionIngresado")
-        let sectionIngreso = document.getElementById("sectionIngreso")
-        sectionIngreso.classList.add("none")
-        let ingresePedido = document.getElementById("ingresePedido")
-        ingresePedido.innerHTML = `BIENVENIDO <u>${nombre.toUpperCase()}</u> INGRESE SU PEDIDO `
-        sectionIngresado.classList.remove("none")
-        let cerrarSesion = document.getElementById("cerrarSesion")
-        return cerrarSesion;
+        $sectionIngreso.classList.add("none")
+        $spinnerCarga.classList.remove("none")
+        setTimeout(() => {
+            $spinnerCarga.classList.add("none")
+            const $ingresePedido = document.getElementById("ingresePedido")
+            $ingresePedido.innerHTML = `INGRESE SU PEDIDO `
+            $sectionIngresado.classList.remove("none")
+            Swal.fire({
+                position: 'center',
+                icon: 'success',
+                title: `Bienvenido <u>${nombre.toUpperCase()}</u> a continuación podrá realizar su pedido`,
+                width: 400,
+                showConfirmButton: false,
+                timer: 3000
+            })
+        }, 2000)
     }
 }
-cerrarSesion.addEventListener("click", e => {
+
+//EVENTOS Y DEMAS
+document.addEventListener("DOMContentLoaded", async e => {
+    await getAllProductos();
+    renderProductos_carrito()
+    renderProductos(); //Muestra productos en HTML
+    guardarProductosLS(productos); //Guarda productos en formato String de la Array "Productos"
+})
+
+document.addEventListener("click", e => {
     if (e.target.matches("#cerrarSesion")) {
-        sectionIngreso.classList.remove("none")
-        sectionIngresado.classList.add("none")
+        Swal.fire({
+            title: '¿Esta seguró de cerrar sesión?',
+            text: "Deberás de completar los campos nuevamente para realizar un pedido.",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#d33',
+            cancelButtonColor: '#3085d6',
+            confirmButtonText: 'Cerrar sesión'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                $sectionIngreso.classList.remove("none");
+                $sectionIngresado.classList.add("none");
+                document.getElementById("numeroCelular").value = "";
+                document.getElementById("nombre").value = "";
+            }
+        })
+    }
+    if (e.target.matches("#vaciarCarrito")) {
+        let cantidadTotal = 0;
+        const productos_carrito = cargarProductosCarrito()
+        productos_carrito.forEach(element => {
+            cantidadTotal += element.cantidad
+        })
+        if (cantidadTotal > 0) {
+            Swal.fire({
+                title: '¡Estas seguro de vaciar tu carrito?',
+                text: "Este paso no puede ser revertido.",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#59C149',
+                cancelButtonColor: '#E05861',
+                confirmButtonText: 'Si, quiero vaciar carrito!',
+                cancelButtonText: 'Cancelar'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    Swal.fire(
+                        '¡Carrito vaciado!',
+                        'Tu carrito ha sido vaciado, esperamos que no te vayas sin comprar :(',
+                        'success'
+                    )
+                    vaciarCarrito();
+                }
+            })
+        } else {
+            Swal.fire(
+                '¡Carrito vacio!',
+                'Tu carrito no tiene productos seleccionados.',
+                'info'
+            )
+        }
+    }
+    if (e.target.matches("#ingresar")) {
+        validarSesion(e);
+    }
+    if (e.target.matches("#finalizarPedido")) {
+        let cantidadTotal = 0;
+        const productos_carrito = cargarProductosCarrito()
+        productos_carrito.forEach(element => {
+            cantidadTotal += element.cantidad
+        })
+        if (cantidadTotal > 0) {
+            let hour = new Date().getHours();
+            let minutes = new Date().getMinutes();
+            Swal.fire({
+                title: '¡Muchas gracias por su compra!',
+                text: `${minutes >= 0 && minutes <= 9 ? `${hour}:0${minutes}` : `Pedido realizado a las ${hour}:${minutes}.
+                        Métodos de pago enviados por Whatsap al numero ${document.getElementById("numeroCelular").value}`}`,
+                imageUrl: './images/logoBakaHeader.png',
+                imageWidth: 200,
+                imageHeight: 200,
+                imageAlt: 'Logo Baka',
+            })
+            $sectionIngreso.classList.remove("none");
+            $sectionIngresado.classList.add("none");
+            document.getElementById("numeroCelular").value = "";
+            document.getElementById("nombre").value = "";
+            vaciarCarrito();
+        } else {
+            Swal.fire(
+                '¡Carrito vacio!',
+                'Tu carrito no tiene productos seleccionados.',
+                'info'
+            )
+            cantidadTotal += element.cantidad
+        }
     }
 })
